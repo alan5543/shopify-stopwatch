26f21829f8b3bdec014c910620fdac00dd1f295f
Alan Yang <alanyang@AlandeMacBook-Pro.local>
2024-01-28 21:31:22 -0500

    implement StopWatch UI

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,8 +1,31 @@
-import { View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import { formatTime } from './utils';
 
-export default function StopWatch() {
+
+// Define the props interface
+interface StopWatchProps {
+  timeElapsed: number;
+}
+
+const StopWatch: React.FC<StopWatchProps> = ({ timeElapsed }) => {
+
   return (
-    <View >
+    <View style={styles.timerContainer}>
+      <Text style={styles.timerText}>{formatTime(timeElapsed)}</Text>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  timerContainer: {
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  timerText: {
+    color: 'black',
+    fontSize: 70,
+    fontWeight: '100',
+  },
+});
+
+export default StopWatch;
---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+// Timer Display
+export const formatTime = (time: number): string => {
+    const minutes = Math.floor(time / 60000) % 60;
+    const seconds = Math.floor(time / 1000) % 60;
+    const milliseconds = Math.floor((time % 1000) / 10);
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}.${milliseconds.toString().padStart(2, '0')}`;
+  };
